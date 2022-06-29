625e81ae0055be29411f6ed6755552e4ac819430
Felix <felix>
2022-06-29 14:30:27 +1000

    Validate form code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const validateTaskForm = () =>{
     let name = document.getElementById("name").value;
     let description = document.getElementById("description").value;
     let formAssigned =  document.getElementById("formAssigned");
-    let dueDate = document.getElementById("date");
+    let dueDate = document.getElementById("date").value;
     let status = document.getElementById("status");
     //Validate Name 
     if(name === ""){
@@ -24,4 +24,25 @@ const validateTaskForm = () =>{
     }else{
         document.getElementById("description").innerHTML = description;
     }
+    //Validate Assigned to
+    if(formAssigned.selectedIndex == 0){
+        alert("Please select Assigning name!!!");
+       // document.getElementById("formAssigned").focus();
+        return false;
+    }
+
+    //validatation for duedate
+    //    var UserDate = document.getElementById("userdate").value;
+        let toDate = null;
+        toDate = new Date(); 
+       if (toDate !== '' || new Date(dueDate).getTime() <= toDate.getTime()) {
+              alert("Please select duedate that is bigger or Equal to today");
+              return false;
+         } 
+
+    //Validate Status
+    if(status.selectedIndex == 0){
+        alert("Please select Status of project");
+       // document.getElementById("formAssigned").focus();
+    } 
 } 
